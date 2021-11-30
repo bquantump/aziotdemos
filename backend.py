7b2fc20099e2b12dc9bb908eb1584884c9f15362
bquantump <stevens@microsoft.com>
2021-11-30 11:40:01 -0800

    wip

---
+++ backend.py
@@ -0,0 +1,51 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific HTTP endpoint on your IoT Hub.
+import sys
+import os
+# pylint: disable=E0611
+
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
+
+from builtins import input
+
+# The service connection string to authenticate with your IoT hub.
+# Using the Azure CLI:
+# az iot hub show-connection-string --hub-name {your iot hub name} --policy-name service
+CONNECTION_STRING = os.environ['IOT_CONNECTION_STRING']
+DEVICE_ID = "pylocaltest"
+
+# Details of the direct method to call.
+METHOD_NAME = "SetTelemetryInterval"
+METHOD_PAYLOAD = "1"
+
+def iothub_devicemethod_sample_run():
+    try:
+        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+        deviceMethod = CloudToDeviceMethod(method_name=METHOD_NAME, payload=METHOD_PAYLOAD)
+        response = registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
+
+        print ( "" )
+        print ( "Device Method called" )
+        print ( "Device Method name       : {0}".format(METHOD_NAME) )
+        print ( "Device Method payload    : {0}".format(METHOD_PAYLOAD) )
+        print ( "" )
+        print ( "Response status          : {0}".format(response.status) )
+        print ( "Response payload         : {0}".format(response.payload) )
+
+        input("Press Enter to continue...\n")
+
+    except KeyboardInterrupt:
+        print ( "" )
+        print ( "IoTHubDeviceMethod sample stopped" )
+
+if __name__ == '__main__':
+    print ( "IoT Hub Python quickstart #2..." )
+    print ( "    Connection string = {0}".format(CONNECTION_STRING) )
+    print ( "    Device ID         = {0}".format(DEVICE_ID) )
+
+    iothub_devicemethod_sample_run()
